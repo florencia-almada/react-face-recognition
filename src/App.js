68622223652a7af06f3242e39cadc81f340c7a78
Florencia <florenciaalmada7@hotmail.com>
2020-03-31 23:48:43 -0300

    Added facial recognition box draft

--- src/App.js
+++ src/App.js
@@ -30,7 +30,8 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
     }
   }
 
@@ -39,20 +40,30 @@ class App extends Component {
     this.setState({input: event.target.value})
   }
 
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box; 
+    const image = document.getElementById('input-image');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    //console.log(box);
+    this.setState({box: box});
+  }
+
   onSubmit = () => {
     this.setState({imageUrl: this.state.input})
     app.models
-      .predict(Clarifai.FACE_DETECT_MODEL,
-              this.state.input)
-      .then(
-              function(response) {
-                console.log(response);
-                console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-              },
-              function(err) {
-                console.log('there was an error!')
-              }
-            );
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log('err'));
   }
 
   render() {
@@ -68,7 +79,7 @@ class App extends Component {
           onInputChange={this.onInputChange} 
           onSubmit={this.onSubmit}  
         />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
